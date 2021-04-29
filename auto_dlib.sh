ae1defa63130783230fdc218d7df6c5ef9102fb4
Mrlin21@gmail.com <Mrlin21@gmail.com>
2021-04-29 19:41:23 +0700

    update

---
+++ auto_dlib.sh
@@ -0,0 +1,18 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install cmake -y
+sudo apt-get install build-essential -y
+sudo apt install libopencv-dev python3-opencv -y
+sudo apt install python3-pip
+#------------------------------install dlib----------------------
+sudo nano auto_install_dlib.sh
+
+sudo git clone https://github.com/davisking/dlib.git
+sudo chmod -R 777 dlib 
+cd dlib
+mkdir build && cd build
+cmake ..
+cmake --build . --config Release
+sudo ldconfig
+sudo make install
+cd ..
+sudo python3 setup.py install 
